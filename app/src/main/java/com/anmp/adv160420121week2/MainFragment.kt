0944ea475391c820b2c42a0182fe8af26693610e
ASUS1 <paddyoop@gmail.com>
2023-03-08 14:21:54 +0700

    finish ppt

---
+++ app/src/main/java/com/anmp/adv160420121week2/MainFragment.kt
@@ -0,0 +1,37 @@
+package com.anmp.adv160420121week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnStart.setOnClickListener {
+            val playerName = txtName.text.toString()
+            val action = MainFragmentDirections.actionGameFragment(playerName)
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
